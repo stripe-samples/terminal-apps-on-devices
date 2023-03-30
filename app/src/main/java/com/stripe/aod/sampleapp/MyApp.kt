374fdc5cc0520679702a1af561372ed271d6f09c
Ian Lin <128665735+ianlin-bbpos@users.noreply.github.com>
2023-03-30 09:59:00 +0800

    initial submit code of basic project. (#2)

---
+++ app/src/main/java/com/stripe/aod/sampleapp/MyApp.kt
@@ -0,0 +1,11 @@
+package com.stripe.aod.sampleapp
+
+import android.app.Application
+import com.stripe.stripeterminal.TerminalApplicationDelegate
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        TerminalApplicationDelegate.onCreate(this)
+    }
+}
