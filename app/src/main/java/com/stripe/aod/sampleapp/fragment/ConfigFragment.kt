9551c17210e9f07cf112a52bfc60c9e9a60ee0e7
eric lin <128665795+EricLin-BBpos@users.noreply.github.com>
2023-04-05 22:43:39 +0800

    add custom configuration fragment (#6)
    * add custom configuration fragment
    * Remove width and height value that directly defined in the theme style
    * Remove RelativeLayout components that used in  layout

---
+++ app/src/main/java/com/stripe/aod/sampleapp/fragment/ConfigFragment.kt
@@ -0,0 +1,36 @@
+package com.stripe.aod.sampleapp.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.stripe.aod.sampleapp.R
+import com.stripe.aod.sampleapp.activity.MainActivity
+import com.stripe.aod.sampleapp.databinding.FragmentConfigBinding
+import com.stripe.aod.sampleapp.utils.backToPrevious
+
+class ConfigFragment : Fragment(R.layout.fragment_config) {
+    companion object {
+        const val TAG = "com.stripe.aod.sampleapp.fragment.ConfigFragment"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // get viewBinding instance
+        val viewBinding = FragmentConfigBinding.bind(view)
+
+        viewBinding.back.setOnClickListener {
+            activity?.backToPrevious()
+        }
+
+        // hand back press action
+        requireActivity().onBackPressedDispatcher.addCallback(
+            activity as MainActivity,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    activity?.backToPrevious()
+                }
+            },
+        )
+    }
+}
