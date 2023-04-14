fcf414c67bdc12a7b86ef232ce13dbb97fd65f02
ian.lin <ian.lin@bbpos.com>
2023-04-14 15:51:05 +0800

    eliminate the argument.

--- app/src/main/java/com/stripe/aod/sampleapp/fragment/ReceiptFragment.kt
+++ app/src/main/java/com/stripe/aod/sampleapp/fragment/ReceiptFragment.kt
@@ -8,21 +8,12 @@ import com.stripe.aod.sampleapp.R
 import com.stripe.aod.sampleapp.databinding.FragmentReceiptBinding
 
 class ReceiptFragment : Fragment(R.layout.fragment_receipt) {
-    companion object {
-        private const val AMOUNT = "com.stripe.aod.sampleapp.fragment.CheckoutFragment.amount"
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewBinding = FragmentReceiptBinding.bind(view)
 
         viewBinding.receiptSkip.setOnClickListener {
             findNavController().popBackStack()
-        }
-
-        arguments?.let {
-            val amountValue = it.getString(AMOUNT)
-            viewBinding.totalAmount.text = amountValue
         }
     }
 }
