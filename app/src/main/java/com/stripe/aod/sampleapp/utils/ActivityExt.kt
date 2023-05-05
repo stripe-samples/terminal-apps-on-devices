502b064e7372e070491f339fe3e73656a6831b5c
eric lin <eric.lin@bbpos.com>
2023-05-05 16:03:33 +0800

    Add handling of global uncaught exceptions.

--- app/src/main/java/com/stripe/aod/sampleapp/utils/ActivityExt.kt
+++ app/src/main/java/com/stripe/aod/sampleapp/utils/ActivityExt.kt
@@ -1,23 +1,33 @@
 package com.stripe.aod.sampleapp.utils
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
 import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.jakewharton.rxbinding4.view.clicks
+import com.stripe.aod.sampleapp.Config
 import com.stripe.aod.sampleapp.R
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import java.text.NumberFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun navOptions(): NavOptions {
@@ -59,9 +69,41 @@ fun EditText.hideKeyboard() {
     imm.hideSoftInputFromWindow(windowToken, 0)
 }
 
+@SuppressLint("CheckResult")
 fun View.setThrottleClickListener(intervalDuration: Long = 1000, block: (view: View) -> Unit) {
     clicks()
         .throttleLast(intervalDuration, TimeUnit.MILLISECONDS)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe { block.invoke(this) }
+}
+
+fun AppCompatActivity.findNavController(): NavController {
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+    return navHostFragment.navController
+}
+
+suspend fun delayAction(delayMillis: Long, action: () -> Unit) {
+    delay(delayMillis)
+    action()
+}
+
+fun handleGlobalException(
+    activityProvider: () -> Activity?,
+    delayInMillis: Long = 500,
+    navigateAction: (NavController) -> Unit
+) {
+    Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+        Log.e(Config.TAG, "handleGlobalException Uncaught exception", throwable)
+
+        val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+        applicationScope.launch(Dispatchers.Main) {
+            delayAction(delayInMillis) {
+                val currentActivity = activityProvider()
+                if (currentActivity != null) {
+                    val navController = (currentActivity as AppCompatActivity).findNavController()
+                    navigateAction(navController)
+                }
+            }
+        }
+    }
 }
