33e8eedae0366ecf53f80f1611c6045c5dd18999
Michael Shafrir <mshafrir@stripe.com>
2023-05-17 21:37:35 -0400

    Convert build scripts to Kotlin DSL
    Also use secrets-gradle-plugin to read values from `local.properties`.

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    namespace = "com.stripe.aod.sampleapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.stripe.aod.sampleapp"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.swiperefreshlayout)
+
+    // ViewModel and LiveData
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel)
+
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.fragment.ktx)
+
+    // OK HTTP
+    implementation(libs.okhttp)
+
+    // Retrofit
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
+    // Stripe Terminal library
+    implementation(libs.stripe.terminal.core)
+    implementation(libs.stripe.terminal.handoffclient)
+
+    // navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+}
