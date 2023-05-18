5e7f1bfa1505dc91f19869336cb43f152eb26cda
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2023-05-18 08:03:41 -0700

    Remove compileOptions (#30)
    This isn't necessary and pins the repo to a specific Java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,6 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
     buildFeatures {
         viewBinding = true
     }
