cef9dc7b18e8940b462e5b9a444203d947a9c7cb
Michael Shafrir <mshafrir@stripe.com>
2023-06-07 15:20:04 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
 
 buildscript {
