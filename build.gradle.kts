3ab6aa22d84d21b23e877b58fa791cd3c6eba5f0
Michael Shafrir <mshafrir@stripe.com>
2024-02-06 10:30:02 -0500

    Bump Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 buildscript {
